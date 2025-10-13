62106d426148a8aa45088693467beee96510fc00
itsobi <obiobialo@aol.com>
2025-10-12 21:08:58 -0500

    fix hydration error

--- components/header/theme-toggle.tsx
+++ components/header/theme-toggle.tsx
@@ -3,10 +3,21 @@
 import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
 import { Button } from '../ui/button';
+import { useEffect, useState } from 'react';
 
 export function ThemeToggle() {
+  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
   const isDark = theme === 'dark';
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
   return (
     <Button
       variant="ghost"
